4542bd819a6fe1d6771f52fd02dea137fe4b2c4f
Basanta Bhandari <basanta17@yahoo.com>
2020-12-21 19:23:54 -0600

    saved

---
+++ src/test/java/com/diamond/testcases/SelectMultipleCheckboxes.java
@@ -0,0 +1,24 @@
+package com.diamond.testcases;
+
+import com.diamond.base.DiamondTestBase;
+import diamondpages.CheckBoxDemoPage;
+import org.testng.Assert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class SelectMultipleCheckboxes extends DiamondTestBase {
+
+
+    public static void main(String[] args) {
+
+        diamondDriver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+        for(int i=1; i<6; i++){
+            System.out.println("(//input[@type='checkbox'])["+i+"]");
+
+            if(!diamondDriver.findElement(By.xpath("(//input[@type='checkbox'])["+i+"]")).isSelected()){
+                diamondDriver.findElement(By.xpath("(//input[@type='checkbox'])["+i+"]")).click();
+            }
+    }
+
+    }
+}
---
+++ src/test/java/com/diamond/testcases/datepickers/DatePickerJS.java
@@ -0,0 +1,30 @@
+package com.diamond.testcases.datepickers;
+
+import com.diamond.base.DiamondTestBase;
+import diamondpages.CheckBoxDemoPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class DatePickerJS extends DiamondTestBase {
+
+    @Test
+    public void runTest1() throws InterruptedException {
+
+        diamondDriver.get("https://www.seleniumeasy.com/test/bootstrap-date-picker-demo.html");
+        WebElement datePicker = diamondDriver.findElement(By.cssSelector("div.input-group.date>input"));
+        datePicker.click();
+        datePicker.clear();
+        String myDate="27/02/1992";
+        enterDateUsingJs(diamondDriver,myDate,datePicker);
+        Thread.sleep(5000);
+
+    }
+
+    private static void enterDateUsingJs(WebDriver driver,String dateStr, WebElement element){
+        JavascriptExecutor js=(JavascriptExecutor) driver;
+        js.executeScript("arguments[0].setAttribute('value', '"+dateStr+"')", element);
+    }
+}
